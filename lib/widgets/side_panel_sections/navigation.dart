d0b73fd0d315fe842fec834f05d3b438fb2fe70e
Zmartt Caracol <zmarttbusiness@gmail.com>
2025-04-30 14:17:40 +0800

    short description. 	+ renamed the asset side panel section folders 		section1 -> navigation 		section3 -> settings 	+ added the 2nd section for side panel 		+ tools 	+ imported flutter svg in navigation file     + created variables to hold assets     + placed the icons for navigation long description.     + renamde the assets/side_panel sub folder to better reflect what they are for and added the 2nd section       and named it tools         + section1 is now navigation         + section3 is not settings         + created a new sub folder named tools     + I started using the assets, I declared them in the pubsec.yaml file and      imported the flutter_svg package that I imported a few commits ago into the navigation      file. I then created 4 final variables to hold the svgpicture assets.         + imported package: flutter_svg         + created 4 new final variables to gold the svgpictures, and used each in the list tiles.

--- lib/widgets/side_panel_sections/navigation.dart
+++ lib/widgets/side_panel_sections/navigation.dart
@@ -1,6 +1,7 @@
 //packages
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Navigation extends StatelessWidget {
 
@@ -14,20 +15,26 @@ class Navigation extends StatelessWidget {
         now for the header. How? Just a container? with some padding? Or maybe another list tile and I'll
         just use the title parameter
     */
-    return Container(
-      color: Colors.amber,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
 
-        children: [
-          ListTile(leading: Text('Leading'), title: Text('test')),
-          ListTile(leading: Text('Leading'), title: Text('test')),
-          ListTile(leading: Text('Leading'), title: Text('test')),
-          SizedBox(height: 50),
-          ListTile(leading: Text('Leading'), title: Text('test'))
-        ],
-      )
+    final SvgPicture codexLogo = SvgPicture.asset('assets/side_panel/navigation/codex_logo_lightmode.svg');
+    
+    final SvgPicture readingIcon = SvgPicture.asset('assets/side_panel/navigation/reading_icon_lightmode.svg');
+
+    final SvgPicture libraryIcon = SvgPicture.asset('assets/side_panel/navigation/library_icon_lightmode.svg');
+
+    final SvgPicture addBookIcon = SvgPicture.asset('assets/side_panel/navigation/add_book_icon_lightmode.svg');
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+    
+      children: [
+        ListTile(leading: codexLogo),
+        ListTile(leading: readingIcon, title: Text('Reading')),
+        ListTile(leading: libraryIcon, title: Text('Library')),
+        SizedBox(height: 50),
+        ListTile(leading: addBookIcon, title: Text('Add Book'))
+      ],
     );
   }
 }
