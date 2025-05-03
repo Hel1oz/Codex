a5833fb2965a644b61bde34a2aee2ab2c5f1d9b4
Zmartt Caracol <zmarttbusiness@gmail.com>
2025-05-03 21:23:40 +0800

    short description.     Made a page navigation model for state control of what page is shown in main view port long description.     I asked Grok and it said that the IndexedStack is probably the best for my situatiom and so     I implemented it in the main view port, I then made a Navigation model, where there's enums which gives the     code a well defined set of pages, and then a map that maps those pages to the index number it has in the     indexedStack.

--- lib/models/navigation_provider.dart
+++ lib/models/navigation_provider.dart
@@ -1,10 +1,23 @@
 //local imports
 
-
 //packages
 import 'package:flutter/material.dart';
 
+enum Pages { readingPage, libraryPage, settingsPage }
+
+const pageIndex = {
+  Pages.readingPage: 0,
+  Pages.libraryPage: 1,
+  Pages.settingsPage: 2,
+};
+
 class NavProvider with ChangeNotifier {
+  int _currentPageIndex = pageIndex[Pages.readingPage] as int;
 
+  int get currentPageIndex => _currentPageIndex;
 
+  void changePage(Pages page) {
+    _currentPageIndex = pageIndex[page] as int;
+    notifyListeners();
+  }
 }
