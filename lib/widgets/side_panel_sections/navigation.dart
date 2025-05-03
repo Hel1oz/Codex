0bb81710ffb81c8d476cf2f051a56daa05def774
Zmartt Caracol <zmarttbusiness@gmail.com>
2025-05-03 21:54:35 +0800

    Just tidied up the imports, organized and added some comments

--- lib/widgets/side_panel_sections/navigation.dart
+++ lib/widgets/side_panel_sections/navigation.dart
@@ -1,6 +1,7 @@
-//packages
-
+//local imports
 import 'package:codex/models/navigation_provider.dart';
+
+//packages
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
