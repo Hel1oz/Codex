830f9f162263c6032f7022aba5f2cc803a2f74aa
Zmartt Caracol <zmarttbusiness@gmail.com>
2025-05-07 18:08:33 +0800

    testing out with PdfViewer

--- lib/routes/main_view_port_pages/reading_page.dart
+++ lib/routes/main_view_port_pages/reading_page.dart
@@ -1,6 +1,6 @@
 //packages
 import 'package:flutter/material.dart';
-
+import 'package:pdfrx/pdfrx.dart';
 
 class ReadingPage extends StatelessWidget {
 
@@ -9,9 +9,13 @@ class ReadingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-      return SizedBox.expand(child: 
-      Container(
-        color: Colors.white
-      ));
+      return SizedBox.expand(
+        child: PdfViewer.file('C:/Users/red/temp/Atlas_Shrugged.pdf',
+        initialPageNumber: 1,
+        params: PdfViewerParams(
+          enableTextSelection: true,
+          
+        ),),
+      );
   }
 }
