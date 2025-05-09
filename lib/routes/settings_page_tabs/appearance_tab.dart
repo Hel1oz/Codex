4350c834a603b5f774a623d603e95b2553ee70d6
Zmartt Caracol <zmarttbusiness@gmail.com>
2025-05-09 15:28:04 +0800

    set up a rudimentary appearance settings tab, will improve later

--- lib/routes/settings_page_tabs/appearance_tab.dart
+++ lib/routes/settings_page_tabs/appearance_tab.dart
@@ -12,22 +12,36 @@ class AppearanceSettingsTab extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement a way for the user to select either dark mode, light mode or system theme.
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisSize: MainAxisSize.min,
+      // mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      // mainAxisSize: MainAxisSize.min,
       children: [
-        Card(
-          color: Theme.of(context).colorScheme.primary,
-          child: Column(children: [
-            Text('Theme Mode'),
-            DropdownButton(
-              value: context.watch<ThemeProvider>().chosenThemeMode,
-              items: const [
-              DropdownMenuItem(value: ThemeMode.light, child: Text('Light')),
-              DropdownMenuItem(value: ThemeMode.dark, child: Text('Dark')),
-              DropdownMenuItem(value: ThemeMode.system, child: Text('System')),
-            ], onChanged: (ThemeMode? value) { context.read<ThemeProvider>().setThemeMode(value!);},),
-          ],)
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Card(
+            shape: ContinuousRectangleBorder(),
+            color: Theme.of(context).colorScheme.primary,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Theme Mode', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+                ),
+                DropdownButton(
+                  padding: EdgeInsets.all(8),
+                  isExpanded: false,
+                  value: context.watch<ThemeProvider>().chosenThemeMode,
+                  items: const [
+                  DropdownMenuItem(value: ThemeMode.light, child: Text('Light')),
+                  DropdownMenuItem(value: ThemeMode.dark, child: Text('Dark')),
+                  DropdownMenuItem(value: ThemeMode.system, child: Text('System')),
+                ], onChanged: (ThemeMode? value) { context.read<ThemeProvider>().setThemeMode(value!);},),
+              ],),
+            )
+          ),
         ),
       ],
     );
